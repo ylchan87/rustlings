65895988396db7829dd922470fd75facb090b6c1
ylchan87 <ylchan87@gmail.com>
2022-08-07 13:09:56 +0800

    at 65 (iterator3)

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,20 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
+// I AM ~NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    //Add your code here
+    fn append_bar(mut self) -> Self{
+        self.push(String::from("Bar"));
+        return self;
+    }
+}
 
 #[cfg(test)]
 mod tests {
